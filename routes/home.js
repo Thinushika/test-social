02310bd8d4711702bf3e56563e8f558ab3462cd9
tj <thinupavani@gmail.com>
2023-06-09 17:23:07 +0530

    code

--- routes/home.js
+++ routes/home.js
@@ -17,21 +17,10 @@ router.post("/", async (req, res, next) => {
   });
 
   router.post("/webhook", (req, res) => {
-    // let body = req.body;
-
-    console.log(`\u{1F7EA} Received webhook:`);
-    // console.dir(body, { depth: null });
-
-
-    // if (body.object === "page") {
-    //     // Returns a '200 OK' response to all requests
-    //     res.status(200).send("EVENT_RECEIVED");
-
-
-    // } else {
-    //     // Return a '404 Not Found' if event is not from a page subscription
-    //     res.sendStatus(404);
-    //   }
+    return res.status(200).json({
+        title: "Express Testing post",
+        message: "The app is working properly!",
+      });
     });
 
 module.exports = router;
