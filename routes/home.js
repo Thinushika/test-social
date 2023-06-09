ae6b6b59b3118656c33c0c9c637c7a8165f46801
tj <thinupavani@gmail.com>
2023-06-09 17:18:07 +0530

    commented

--- routes/home.js
+++ routes/home.js
@@ -17,21 +17,21 @@ router.post("/", async (req, res, next) => {
   });
 
   router.post("/webhook", (req, res) => {
-    let body = req.body;
+    // let body = req.body;
 
     console.log(`\u{1F7EA} Received webhook:`);
-    console.dir(body, { depth: null });
+    // console.dir(body, { depth: null });
 
 
-    if (body.object === "page") {
-        // Returns a '200 OK' response to all requests
-        res.status(200).send("EVENT_RECEIVED");
+    // if (body.object === "page") {
+    //     // Returns a '200 OK' response to all requests
+    //     res.status(200).send("EVENT_RECEIVED");
 
 
-    } else {
-        // Return a '404 Not Found' if event is not from a page subscription
-        res.sendStatus(404);
-      }
+    // } else {
+    //     // Return a '404 Not Found' if event is not from a page subscription
+    //     res.sendStatus(404);
+    //   }
     });
 
 module.exports = router;
