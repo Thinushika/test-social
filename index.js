b565224b15ae3561096970cec3eb1bab442c3727
tj <thinupavani@gmail.com>
2023-06-09 17:06:05 +0530

    update

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const express = require("express");
-const cors = require("cors");
+const home = require("./routes/home");
+const cors = require('cors');
 
-require('dotenv').config();
+// Middlewares
 const app = express();
-const port = process.env.PORT || 5000;
 app.use(cors());
+app.use(express.json());
+
+// Routes
 
 
-app.get('/', (req, res) => {
-    console.log("Welcome")
-})
+app.use("/home", home);
 
-app.listen(port, () => {
-    console.log(`Server started at http://localhost:${port}`);
-});
-
-
-module.exports = app
+// connection
+const port = process.env.PORT || 9001;
+app.listen(port, () => console.log(`Listening to port ${port}`));
